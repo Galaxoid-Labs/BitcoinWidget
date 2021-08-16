5e0bba4ad6939bd50955063c855ab933ce667bb7
Jacob <ismyhc@gmail.com>
2021-08-16 16:28:33 -0400

    Initial Commit

---
+++ Shared/BitcoinWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitcoinWidgetApp.swift
+//  Shared
+//
+//  Created by Jacob Davis on 8/16/21.
+//
+
+import SwiftUI
+
+@main
+struct BitcoinWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
